da447c3a0774a0269ff9ad2e6bb1285376530658
ChencongDiu <chencong@gwmail.gwu.edu>
2017-11-15 22:18:41 -0500

    update

--- snack/src/script/snake.js
+++ snack/src/script/snake.js
@@ -2,19 +2,50 @@
 * @Author: x
 * @Date:   2017-11-15 18:58:42
 * @Last Modified by:   x
-* @Last Modified time: 2017-11-15 19:18:51
+* @Last Modified time: 2017-11-15 22:17:30
 */
-var jsDiv = document.getElementById("window");
-var jsSnack = document.getElementById("snack");
-var jsStart = document.getElementById("start");
-var jsFood = document.getElementById("food");
 var jsBody = document.getElementById("world");
+var jsSnack = document.getElementById("snack");
+var jsFood = document.getElementById("food");
 var jsScore = document.getElementById("score");
 var timer;
-var timer1 = setInterval(eat,10); //check collision and eat
+var speed = 300;
+var checkEat = setInterval(eat,speed); //check eating
 var srr = []; //tracker
 var num = 0; //length of arr
-var jsSnackBody; //length of snake
+
+function eat(){
+    checkEating(jsSnack,jsFood);
+    function checkEating(snake, food) {
+    	//distance from object's left to world left
+        var snakeL = snake.offsetLeft;
+        var snakeT = snake.offsetTop;
+        var foodL = food.offsetLeft;
+        var foodT = food.offsetTop;
+        
+        //check eating, snake and food are overlapping
+        if (snakeL == foodL && snakeT == foodT) {
+        	//create a body
+            newBody = document.createElement("div");
+            newBody.setAttribute("class","body");
+            jsBody.appendChild(newBody);
+            Pos();
+            speed *= 0.95
+            setInterval(follow,speed);
+        }
+    }
+
+    function follow(){
+        var bodyNum = document.getElementsByClassName("body");
+        jsScore.innerHTML = bodyNum.length;
+        var place = 0 ;
+        for(var i = 0 ; i<bodyNum.length ; i++){
+            place += 1;
+            bodyNum[i].style.left=srr[num-place][0] + 'px';
+            bodyNum[i].style.top=srr[num-place][1] + 'px';    
+        }
+    }
+}
 
 document.onkeydown = function(e) {
 	var evt = e || window.event;
@@ -22,10 +53,10 @@ document.onkeydown = function(e) {
 		//left 37
 		case 37:
 			clearInterval(timer);
-            timer=window.setInterval(runLeft,10)
+            timer = window.setInterval(runLeft,speed)
             function runLeft(){
-                if (jsSnack.offsetLeft > 0) {
-                    jsSnack.style.left = jsSnack.offsetLeft - 1 + "px";
+                if (jsSnack.offsetLeft >= 24) {
+                    jsSnack.style.left = jsSnack.offsetLeft - 24 + "px";
                     jsSnack.style.top = jsSnack.offsetTop + "px";
                     srr.push([jsSnack.offsetLeft, jsSnack.offsetTop]);
                     num++;
@@ -35,10 +66,10 @@ document.onkeydown = function(e) {
 		//up 38
 		case 38:
 			clearInterval(timer);
-            timer=window.setInterval(runTop,10)
+            timer=window.setInterval(runTop,speed)
             function runTop(){
-                if (jsSnack.offsetLeft > 0) {
-                    jsSnack.style.top = jsSnack.offsetTop - 1 + "px";
+                if (jsSnack.offsetLeft >= 24) {
+                    jsSnack.style.top = jsSnack.offsetTop - 24 + "px";
                     jsSnack.style.left = jsSnack.offsetLeft + "px";
                     srr.push([jsSnack.offsetLeft, jsSnack.offsetTop]);
                     num++;
@@ -48,10 +79,10 @@ document.onkeydown = function(e) {
 		//right 39
 		case 39:
 			clearInterval(timer);
-            timer=window.setInterval(runRight,10);
+            timer=window.setInterval(runRight,speed);
             function runRight(){
-                if (jsSnack.offsetLeft + jsSnack.offsetWidth <= 450) {
-                    jsSnack.style.left = jsSnack.offsetLeft + 1 + "px";
+                if (jsSnack.offsetLeft + jsSnack.offsetWidth <= 456) {
+                    jsSnack.style.left = jsSnack.offsetLeft + 24 + "px";
                     jsSnack.style.top = jsSnack.offsetTop + "px";
                     srr.push([jsSnack.offsetLeft, jsSnack.offsetTop]);
                     num++;
@@ -61,10 +92,10 @@ document.onkeydown = function(e) {
 		//down 40
 		case 40:
 			clearInterval(timer);
-            timer=window.setInterval(runBottom,10);            
+            timer=window.setInterval(runBottom,speed);            
             function runBottom(){
-                if (jsSnack.offsetTop + jsSnack.offsetHeight <= 400) {
-                    jsSnack.style.top = jsSnack.offsetTop + 1 + "px";
+                if (jsSnack.offsetTop + jsSnack.offsetHeight <= 456) {
+                    jsSnack.style.top = jsSnack.offsetTop + 24 + "px";
                     jsSnack.style.left = jsSnack.offsetLeft + "px";
                     srr.push([jsSnack.offsetLeft, jsSnack.offsetTop]);
                     num++;
@@ -74,40 +105,12 @@ document.onkeydown = function(e) {
 	}
 }
 
+//generate food randomly
 function Pos(){
-    jsFood.style.left=parseInt(Math.random() * (430 - 20 + 1) + 20) + "px";
-    jsFood.style.top=parseInt(Math.random() * (380 - 20 + 1) + 20) + "px";
+	//random int [0, 19]
+	var ranL = parseInt(Math.random() * 20);
+	var ranT = parseInt(Math.random() * 20);
+    jsFood.style.left = ranL * 24 + "px";
+    jsFood.style.top = ranT * 24 + "px";
 }
 Pos();
-
-function eat(){
-    rectangleCrashExamine(jsSnack,jsFood);
-    function rectangleCrashExamine(obj1, obj2) {
-        var obj1Left = obj1.offsetLeft;
-        var obj1Width = obj1.offsetLeft + obj1.offsetWidth;
-        var obj1Top = obj1.offsetTop;
-        var obj1Height = obj1.offsetTop + obj1.offsetHeight;
-        var obj2Left = obj2.offsetLeft;
-        var obj2Width = obj2.offsetLeft + obj2.offsetWidth;
-        var obj2Top = obj2.offsetTop;
-        var obj2Height = obj2.offsetTop + obj2.offsetHeight;
-        
-        if ( !(obj1Left > obj2Width || obj1Width < obj2Left || obj1Top > obj2Height || obj1Height < obj2Top) ) {
-            jsSnackBody = document.createElement("div");
-            jsSnackBody.setAttribute("class","body");
-            jsBody.appendChild(jsSnackBody);
-            setInterval(follow,10);
-        }
-    }
-
-    function follow(){
-        var bodyNum = document.getElementsByClassName("body");
-        jsScore.innerHTML = bodyNum.length;
-        var place = 0 ;
-        for( var i = 0 ; i<bodyNum.length ; i++){
-            place += 20;
-            bodyNum[i].style.left=srr[num-place][0] + 'px';
-            bodyNum[i].style.top=srr[num-place][1] + 'px';    
-        }
-    }
-}
