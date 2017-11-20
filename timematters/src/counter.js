65233d833a44299bdc6c7e2af8ea50d2847149ac
ChencongDiu <chencong@gwmail.gwu.edu>
2017-11-19 21:19:04 -0500

    update

---
+++ timematters/src/counter.js
@@ -0,0 +1,74 @@
+/*
+* @Author: x
+* @Date:   2017-11-19 19:18:11
+* @Last Modified by:   x
+* @Last Modified time: 2017-11-19 21:15:58
+*/
+var WINDOW_WIDTH  = 1024;
+var WINDOW_HEIGHT = 768;
+var RADIUS = 8;
+var MARGIN_TOP  = 60;
+var MARGIN_LEFT = 30;
+var START = 0;
+var END   = 2 * Math.PI;
+
+const endTime = new Date(2017, 10, 19, 23, 59);
+var curShowTimeSeconds = 0;
+
+window.onload = function() {
+	var canvas = document.getElementById('canvas');
+	var ctx = canvas.getContext('2d');
+
+	canvas.width = WINDOW_WIDTH;
+	canvas.height = WINDOW_HEIGHT;
+
+	curShowTimeSeconds = getCurShowTimeSeconds();
+	render(ctx);
+};
+
+function getCurShowTimeSeconds() {
+	var curTime = new Date();
+	var ret = endTime.getTime() - curTime.getTime();
+	ret = Math.round(ret / 1000);
+
+	return ret >= 0? ret: 0;
+}
+
+function render(ctx) {
+	var hours = parseInt(curShowTimeSeconds / 3600);
+	var minutes = parseInt((curShowTimeSeconds - hours * 3600) / 60);
+	var seconds = parseInt(curShowTimeSeconds % 60);
+	console.log(hours, minutes, seconds);
+
+	renderDigit(MARGIN_LEFT, MARGIN_TOP, parseInt(hours / 10), ctx);
+	renderDigit(MARGIN_LEFT + 15 * (RADIUS + 1), MARGIN_TOP, parseInt(hours % 10), ctx);
+	renderDigit(MARGIN_LEFT + 30 * (RADIUS + 1), MARGIN_TOP, 10, ctx);
+	renderDigit(MARGIN_LEFT + 39 * (RADIUS + 1), MARGIN_TOP, parseInt(minutes / 10), ctx);
+	renderDigit(MARGIN_LEFT + 54 * (RADIUS + 1), MARGIN_TOP, parseInt(minutes % 10), ctx);
+	renderDigit(MARGIN_LEFT + 69 * (RADIUS + 1), MARGIN_TOP, 10, ctx);
+	renderDigit(MARGIN_LEFT + 78 * (RADIUS + 1), MARGIN_TOP, parseInt(seconds / 10), ctx);
+	renderDigit(MARGIN_LEFT + 93 * (RADIUS + 1), MARGIN_TOP, parseInt(seconds % 10), ctx);
+};
+
+function renderDigit(x, y, num, ctx) {
+	ctx.fillStyle = "rgb(0, 102, 153)";
+
+	for (var i = 0; i < digit[num].length; i++) {
+		for (var j = 0; j < digit[num][i].length; j++) {
+			if (digit[num][i][j] == 1) {
+				var xy = digitxy(x, y, i, j);
+				ctx.beginPath();
+				ctx.arc(xy[0], xy[1], RADIUS, START, END);
+				ctx.closePath();
+
+				ctx.fill();
+			}
+		}
+	}
+};
+
+function digitxy(x, y, i, j) {
+	var digitx = x + j * 2 * (RADIUS + 1) + (RADIUS + 1);
+	var digity = y + i * 2 * (RADIUS + 1) + (RADIUS + 1);
+	return [digitx, digity];
+};
